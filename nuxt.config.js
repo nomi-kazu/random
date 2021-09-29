19ebb51e7f3b0a1f0b2676327a9f94fa4cd214ec
nomi-kazu <nomikazyu@gmail.com>
2021-09-29 15:59:22 +0900

    no message

--- nuxt.config.js
+++ nuxt.config.js
@@ -88,9 +88,5 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     extend(config, ctx) {}
-  },
-  generate: { 
-    dir: 'public' 
-  },
-  target: 'static',
+  }
 }
