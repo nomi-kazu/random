7c59df8870e32b7dd8b94bb409f271014c02c34e
nomi-kazu <nomikazyu@gmail.com>
2021-11-14 21:38:13 +0900

    feat: success時に返す各UserSerializerを設定

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-        registrations: 'api/v1/auth/registrations'
+        registrations: 'api/v1/auth/registrations',
+        sessions: 'api/v1/auth/sessions'
       }
     end
+    resources :users, only: [:show]
   end
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
