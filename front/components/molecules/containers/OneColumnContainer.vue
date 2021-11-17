dfd56a23924b1a5288acb3f5fdbb88f8b4473f3e
nomi-kazu <nomikazyu@gmail.com>
2021-11-17 23:05:51 +0900

    feat: containerを追加

---
+++ front/components/molecules/containers/OneColumnContainer.vue
@@ -0,0 +1,14 @@
+<template>
+  <BaseContainer>
+    <v-row justify="center">
+      <v-col>
+        <slot />
+      </v-col>
+    </v-row>
+  </BaseContainer>
+</template>
+
+<script>
+export default {
+}
+</script>
