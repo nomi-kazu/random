b34969dbc35814a5ddb67b6c23b256ca434dec11
nomi-kazu <nomikazyu@gmail.com>
2021-11-21 19:45:53 +0900

    feat: universal-cookieの追加、簡易ログインフォームを作成

--- front/pages/signin/index.vue
+++ front/pages/signin/index.vue
@@ -1,10 +1,48 @@
 <template>
-  <div>
-    <SigninTemplate />
-  </div>
+  <v-app>
+    <v-card width="600px" class="mx-auto mt-5">
+      <v-card-text>
+        <v-form>
+          <v-text-field
+            v-model="email"
+            label="メール"
+          />
+
+          <v-text-field
+            v-model="password"
+            :rules="[rules.required, rules.min]"
+            :type="showPassword ? 'text' : 'password'"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            label="パスワード"
+            counter
+            @click:append="showPassword = !showPassword"
+          />
+
+          <v-card-actions>
+            <v-btn class="info" large block>
+              ログイン
+            </v-btn>
+          </v-card-actions>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-app>
 </template>
 
 <script>
 export default {
+  data: () => ({
+    showPassword: false,
+    password: '',
+    email: '',
+    rules: {
+      required: (value) => {
+        return !!value || 'Required.'
+      },
+      min: (value) => {
+        return value.length >= 8 || 'Min 8 characters'
+      }
+    }
+  })
 }
 </script>
