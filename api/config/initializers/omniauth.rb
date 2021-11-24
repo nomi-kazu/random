35e0ff3247542dd5910ca32d09ce395d66a18c65
nomi-kazu <nomikazyu@gmail.com>
2021-11-24 19:38:37 +0900

    feat: omniauthにfull_hostを設定

--- api/config/initializers/omniauth.rb
+++ api/config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  OmniAuth.config.full_host = ENV["API_DOMAIN"]
   # provider :twitter,      ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   # provider :instagram,    ENV['INSTAGRAM_KEY'], ENV['INSTAGRAM_SECRET']
   # provider :facebook,     ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
