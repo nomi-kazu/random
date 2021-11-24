6282b319aced80075bea648bb081423609877ad4
nomi-kazu <nomikazyu@gmail.com>
2021-11-24 21:13:06 +0900

    feat: serializerの設定

--- api/app/serializers/user_serializer.rb
+++ api/app/serializers/user_serializer.rb
@@ -1,3 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
+  include Rails.application.routes.url_helpers
+
   attributes :id, :name, :username, :image, :profile, :address, :twitter_link, :instagram_link, :facebook_link, :created_at, :updated_at
+
+  def image
+    object.avatar.attached? ? url_for(object.avatar) : object.image
+  end
 end
