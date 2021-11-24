0a5c1ee57e568e7397e969e75d8434804ae59e4a
nomi-kazu <nomikazyu@gmail.com>
2021-11-24 18:55:46 +0900

    fix: ユーザーのshowアクションのパラメータをusernameに変更

--- api/app/controllers/api/v1/users_controller.rb
+++ api/app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::UsersController < ApplicationController
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by(username: params[:username])
     render json: @user, serializer: UserSerializer
   end
 
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'users/:username', to: 'users#show'
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         registrations: 'api/v1/auth/registrations',
         sessions: 'api/v1/auth/sessions',
         omniauth_callbacks: 'api/v1/auth/omniauth_callbacks'
       }
-      resources :users, only: [:show]
     end
   end
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
