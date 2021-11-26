25538aee974422140bc7e015a3200713cf2c8ce1
nomi-kazu <nomikazyu@gmail.com>
2021-11-26 23:36:36 +0900

    feat: UserSnsBtnGroupを作成

--- front/components/organisms/btnGroup/UserSnsBtnGroup.vue
+++ front/components/organisms/btnGroup/UserSnsBtnGroup.vue
@@ -1,10 +1,26 @@
 <template>
   <div>
-    <!-- snsを紐付けるボタンをまとめる -->
+    <TwitterBtn fab v-if="twitterLinkIsExists" />
+    <InstagramBtn fab v-if="instagramLinkIsExists" />
+    <FacebookBtn fab v-if="facebookLinkIsExists" />
   </div>
 </template>
 
 <script>
 export default {
+  computed: {
+    twitterLinkIsExists () {
+      // Twitterリンクが存在したらtrueを返す
+      return false
+    },
+    instagramLinkIsExists () {
+      // Instagramリンクが存在したらtrueを返す
+      return false
+    },
+    facebookLinkIsExists () {
+      // Instagramリンクが存在したらtrueを返す
+      return false
+    }
+  }
 }
 </script>
