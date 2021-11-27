4ccca7109ee7cb1cda304875da32dc44c5aefa7b
nomi-kazu <nomikazyu@gmail.com>
2021-11-27 21:40:04 +0900

    feat: ユーザー情報を取得、表示

---
+++ front/components/organisms/cards/UserIntroCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-card>
+    <div>
+      <v-img src="https://picsum.photos/510/300?random" class="user-icon" />
+    </div>
+
+    <p class="name">{{ userInfo.attributes.name }}</p>
+
+    <p class="profile">{{ userInfo.attributes.profile }}</p>
+
+    <div>
+      <p>{{ userInfo.attributes.address }}</p>
+
+      <UserSnsBtnGroup />
+    </div>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    userInfo: {
+      type: Object,
+      default: undefined
+    }
+  }
+}
+</script>
+
+<style scoped>
+.user-icon {
+  width: 80px;
+  height: 80px;
+  object-fit: cover;
+  border-radius: 50%;
+}
+.name {
+  font-size: 28px;
+  font-weight: bold;
+}
+.profile {
+  width: 80%;
+}
+</style>
--- front/pages/_username/index.vue
+++ front/pages/_username/index.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
-    <p>ユーザー名：{{ username }}</p>
+    <UsernameIndexTemplate :user-info="userInfo" />
   </div>
 </template>
 
 <script>
 export default {
+  async asyncData ({ $axios, params }) {
+    try {
+      const { data } = await $axios.$get(process.env.API_URL + '/api/v1/users/1')
+      console.log(data)
+      return { userInfo: data }
+    } catch (e) {
+      console.error(e)
+    }
+  },
+
   computed: {
     username () {
       return this.$route.params.username
